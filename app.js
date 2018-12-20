53ba32e38c4f88d81f6d201b082d240a43f06499
Jacob Niedzwiecki <jacob@jakemoves.com>
2018-12-20 16:38:00 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const WebSocket = require('ws')
+
+const client = new WebSocket('ws://localhost:3000')
+
+client.addEventListener('open', () => {
+  console.log('connection open')
+})
+
+client.addEventListener('message', (msg) => {
+  console.log(msg.data)
+})
+
+client.addEventListener('close', () => {
+  console.log('connection closed')
+})
+
+client.addEventListener('error', (err) => {
+  console.log(error)
+})
