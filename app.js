eede4c95f803fe07c84dda0fe1f2fe31af04971a
Jacob Niedzwiecki <jacob@jakemoves.com>
2018-12-21 09:29:59 -0500

    Updated for new guid protocol

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const WebSocket = require('ws')
+const uuid = require('uuid/v4')
 
 const client = new WebSocket('ws://localhost:3000')
 
 client.addEventListener('open', () => {
   console.log('connection open')
+  client.send(JSON.stringify({ "guid": "" + uuid()}))
 })
 
 client.addEventListener('message', (msg) => {
