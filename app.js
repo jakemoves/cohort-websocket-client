de2a82f0fa1c5bf9e650483fc06d20d0f819b97b
Jacob Niedzwiecki <jacob@jakemoves.com>
2019-01-24 18:37:45 -0500

    added command line flag --guid

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 const WebSocket = require('ws')
-const uuid = require('uuid/v4')
+var argv = require('minimist')(process.argv.slice(2));
 
-const client = new WebSocket('ws://localhost:3000')
+// const client = new WebSocket('https://cohort.rocks/sockets') // production 
+const client = new WebSocket('http://localhost:3000/sockets') // development
+
+const args = process.argv.slice(2)
+
+const guid = argv.guid
 
 client.addEventListener('open', () => {
   console.log('connection open')
-  client.send(JSON.stringify({ "guid": "" + 12345})//uuid()}))
+  client.send(JSON.stringify({ "guid": guid, "eventId": 3}))
 })
 
 client.addEventListener('message', (msg) => {
   console.log(msg.data)
 })
 
-client.addEventListener('close', () => {
-  console.log('connection closed')
+client.addEventListener('close', (msg) => {
+  console.log('connection closed with error ' + msg.code + ': ' + msg.reason)
 })
 
 client.addEventListener('error', (err) => {
