8907f99b82e3f2454a14f91c49dde45529348150
Jacob Niedzwiecki <jake@jacob-n.com>
2019-11-27 18:12:19 -0700

    Tweaks

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const WebSocket = require('ws')
+const guid = require('uuid/v4')
+
 var argv = require('minimist')(process.argv.slice(2));
 
 const args = process.argv.slice(2)
 
-const guid = argv.guid
-const eventId = argv.event
+const deviceGuid = argv.guid || guid()
+const occasionId = argv.occasion
 const env = argv.env || 'dev'
 
 var client
@@ -12,11 +14,14 @@ if(env == 'dev'){
   client = new WebSocket('http://localhost:3000/sockets') // development
 } else if(env == 'prod'){
   client = new WebSocket('https://cohort.rocks/sockets') // production 
+} else if(env == 'staging' ){
+  client = new WebSocket('https://staging.cohort.rocks/sockets') // staging
 }
 
 client.addEventListener('open', () => {
   console.log('connection open')
-  client.send(JSON.stringify({ "guid": "" + guid, "eventId": eventId}))
+  
+  client.send(JSON.stringify({ "guid": "" + deviceGuid, "occasionId": occasionId}))
 })
 
 client.addEventListener('message', (msg) => {
